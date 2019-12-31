8a2416077513fcbcba4333268f32b3ce57019a40
Jeanine Hoffman <jeanine.hoffman1970@gmail.com>
2019-12-31 10:50:09 -0800

    added some stuff

---
+++ src/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  // API_BASE_URL: process.env.REACT_APP_API_BASE_URL || `https://communitytoolbox.herokuapp.com/api`,
+  // REACT_APP_TOKEN_KEY: 'community-toolbox-auth-token',
+  // REACT_APP_BASKET_KEY: 'community-toolbox-basket',
+  // REACT_APP_USER_ID_KEY: 'community-toolbox-logged-in-user',
+}
